3a667fdd99ee3e95d219d22e4a1935abb9d8e5e9
Vaidulla <vajdullaak2@gmail.com>
2026-04-07 13:19:54 +0300

    Отправка отчета

---
+++ DelegateExampleApp/DelegateExampleApp/Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace DelegateExampleApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(11, 51);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(131, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Исходный текст";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(11, 104);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(90, 32);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Изменить текст";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(286, 206);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Изменение текста с делегатом";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ DelegateExampleApp/DelegateExampleApp/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Reflection.Emit;
+using System.Windows.Forms;
+
+namespace DelegateExampleApp
+{
+    public partial class Form1 : Form
+    {
+        // Объявление делегата
+        public delegate void ChangeTextDelegate(string newText);
+
+        // Объявляем переменную делегата на уровне класса
+        ChangeTextDelegate changeTextDel;
+        Random random = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Метод для изменения текста на приветствие
+        private void ChangeTextToHello(string newText)
+        {
+            label1.Text = "Текст изменён на: " + newText;
+        }
+
+        // Метод для изменения текста на "Добро пожаловать!"
+        private void ChangeTextToWelcome(string newText)
+        {
+            label1.Text = "Добро пожаловать!";
+        }
+
+        // Метод для изменения текста на "Как дела?"
+        private void ChangeTextToHowAreYou(string newText)
+        {
+            label1.Text = "Как дела?";
+        }
+
+        // Обработчик события нажатия кнопки
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Случайный выбор метода (от 1 до 3)
+            int methodIndex = random.Next(1, 4);
+
+            // Привязка делегата к выбранному методу
+            switch (methodIndex)
+            {
+                case 1:
+                    changeTextDel = ChangeTextToHello;
+                    break;
+                case 2:
+                    changeTextDel = ChangeTextToWelcome;
+                    break;
+                case 3:
+                    changeTextDel = ChangeTextToHowAreYou;
+                    break;
+                default:
+                    changeTextDel = ChangeTextToHello;
+                    break;
+            }
+
+            // Вызов метода через делегат
+            changeTextDel("Новый текст");
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
